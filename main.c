d383b95f253dd49031cf8236714882fbd7b069d8
f01zy <aminovali015@icloud.com>
2026-04-05 21:38:24 +0300

    added depth buffer

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <unistd.h>
 
+#define MAX_ROWS 1024
+#define MAX_COLS 1024
+
 struct Vertex {
   float x;
   float y;
@@ -12,12 +15,13 @@ struct Vertex {
 
 float A, B, C;
 const char ch = '#';
-const float face = 30.0f;
+const float far = 1e9f;
+const float face = 35.0f;
 const float mid = face / 2.0f;
-struct Vertex vertices[8] = {
-    {-mid, -mid, mid}, {-mid, -mid, -mid}, {mid, -mid, -mid}, {mid, -mid, mid},
-    {-mid, mid, mid},  {-mid, mid, -mid},  {mid, mid, -mid},  {mid, mid, mid},
-};
+
+float buffer[MAX_ROWS][MAX_COLS];
+struct Vertex vertices[8] = {{-mid, -mid, mid}, {-mid, -mid, -mid}, {mid, -mid, -mid}, {mid, -mid, mid},
+                             {-mid, mid, mid},  {-mid, mid, -mid},  {mid, mid, -mid},  {mid, mid, mid}};
 
 void rotate_x(struct Vertex *vertex) {
   float y, z;
@@ -43,9 +47,15 @@ void rotate_z(struct Vertex *vertex) {
   vertex->y = y;
 }
 
+void update_depth_buffer(int x, int y, float z) {
+  float curr = buffer[y][x];
+  buffer[y][x] = curr > z ? z : curr;
+}
+
 void draw_line(struct Vertex a, struct Vertex b) {
   int cols, rows;
   getmaxyx(stdscr, rows, cols);
+  int az = a.z;
   int ax = cols / 2 + (int)a.x;
   int ay = rows / 2 + (int)a.y / 2;
   int bx = cols / 2 + (int)b.x;
@@ -55,9 +65,13 @@ void draw_line(struct Vertex a, struct Vertex b) {
   int signX = ax < bx ? 1 : -1;
   int signY = ay < by ? 1 : -1;
   int error = deltaX - deltaY;
-  mvaddch(by, bx, ch);
-  while (ax != bx || ay != by) {
-    mvaddch(ay, ax, ch);
+  int steps = deltaX > deltaY ? deltaX : deltaY;
+  float step = (b.z - a.z) / steps;
+
+  while (1) {
+    update_depth_buffer(ax, ay, az);
+    az += step;
+    if (ax == bx && ay == by) break;
     int temp = error * 2;
     if (temp > -deltaY) {
       error -= deltaY;
@@ -82,8 +96,12 @@ int main() {
       rotate_y(&temp[i]);
       rotate_z(&temp[i]);
     }
-    clear();
-    mvprintw(0, 0, "X: %f, Y: %f, Z: %f\n", A, B, C);
+    for (int i = 0; i < MAX_ROWS; i++) {
+      for (int j = 0; j < MAX_COLS; j++) {
+        buffer[i][j] = far;
+      }
+    }
+
     draw_line(temp[0], temp[1]);
     draw_line(temp[1], temp[2]);
     draw_line(temp[2], temp[3]);
@@ -96,10 +114,23 @@ int main() {
     draw_line(temp[1], temp[5]);
     draw_line(temp[2], temp[6]);
     draw_line(temp[3], temp[7]);
+
+    int cols, rows;
+    getmaxyx(stdscr, rows, cols);
+    clear();
+    mvprintw(0, 0, "rotation (radians)\n");
+    mvprintw(1, 0, "x: %.1f, y: %.1f, z: %.1f\n", A, B, C);
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        float depth = buffer[i][j];
+        if (depth != far) mvaddch(i, j, '#');
+      }
+    }
     refresh();
-    A += 0.2f;
-    B += 0.2f;
-    C += 0.2f;
+
+    A += 0.1f;
+    B += 0.1f;
+    C += 0.1f;
     usleep(100000);
   }
   endwin();
