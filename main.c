fd28575335984084127a3231d5da3e55819757d8
f01zy <aminovali015@icloud.com>
2026-04-06 14:45:01 +0300

    added normales

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #define MAX_ROWS 1024
 #define MAX_COLS 1024
 
-struct Vertex {
+struct Vec3 {
   float x;
   float y;
   float z;
@@ -20,31 +20,42 @@ const float face = 35.0f;
 const float mid = face / 2.0f;
 
 float buffer[MAX_ROWS][MAX_COLS];
-struct Vertex vertices[8] = {{-mid, -mid, mid}, {-mid, -mid, -mid}, {mid, -mid, -mid}, {mid, -mid, mid},
-                             {-mid, mid, mid},  {-mid, mid, -mid},  {mid, mid, -mid},  {mid, mid, mid}};
+struct Vec3 camera = {0, 0, 1};
+struct Vec3 vertices[8];
+struct Vec3 normales[6];
+struct Vec3 base_vertices[8] = {{-mid, -mid, mid}, {-mid, -mid, -mid}, {mid, -mid, -mid}, {mid, -mid, mid},
+                                {-mid, mid, mid},  {-mid, mid, -mid},  {mid, mid, -mid},  {mid, mid, mid}};
 
-void rotate_x(struct Vertex *vertex) {
-  float y, z;
-  y = vertex->y * cos(A) - vertex->z * sin(A);
-  z = vertex->y * sin(A) + vertex->z * cos(A);
-  vertex->y = y;
-  vertex->z = z;
+void rotate_x(struct Vec3 *vertex) {
+  float verticesY = vertex->y, verticesZ = vertex->z;
+  vertex->y = verticesY * cos(A) - verticesZ * sin(A);
+  vertex->z = verticesY * sin(A) + verticesZ * cos(A);
 }
 
-void rotate_y(struct Vertex *vertex) {
-  float x, z;
-  x = vertex->x * cos(B) + vertex->z * sin(B);
-  z = vertex->z * cos(B) - vertex->x * sin(B);
-  vertex->x = x;
-  vertex->z = z;
+void rotate_y(struct Vec3 *vertex) {
+  float verticesX = vertex->x, verticesZ = vertex->z;
+  vertex->x = verticesX * cos(B) + verticesZ * sin(B);
+  vertex->z = verticesZ * cos(B) - verticesX * sin(B);
 }
 
-void rotate_z(struct Vertex *vertex) {
-  float x, y;
-  x = vertex->x * cos(C) - vertex->y * sin(C);
-  y = vertex->x * sin(C) + vertex->y * cos(C);
-  vertex->x = x;
-  vertex->y = y;
+void rotate_z(struct Vec3 *vertex) {
+  float verticesX = vertex->x, verticesY = vertex->y;
+  vertex->x = verticesX * cos(C) - verticesY * sin(C);
+  vertex->y = verticesX * sin(C) + verticesY * cos(C);
+}
+
+int dot_vec3(struct Vec3 a, struct Vec3 b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+struct Vec3 subtract_vec3(struct Vec3 a, struct Vec3 b) { return (struct Vec3){a.x - b.x, a.y - b.y, a.z - b.z}; }
+struct Vec3 cross_vec3(struct Vec3 a, struct Vec3 b) { return (struct Vec3){a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x}; }
+struct Vec3 get_normale(struct Vec3 a, struct Vec3 b, struct Vec3 c) { return cross_vec3(subtract_vec3(b, a), subtract_vec3(c, a)); }
+
+void configure_normales() {
+  normales[0] = get_normale(vertices[2], vertices[1], vertices[0]);
+  normales[1] = get_normale(vertices[4], vertices[5], vertices[6]);
+  normales[2] = get_normale(vertices[4], vertices[0], vertices[1]);
+  normales[3] = get_normale(vertices[5], vertices[1], vertices[2]);
+  normales[4] = get_normale(vertices[2], vertices[3], vertices[7]);
+  normales[5] = get_normale(vertices[3], vertices[0], vertices[4]);
 }
 
 void update_depth_buffer(int x, int y, float z) {
@@ -52,7 +63,7 @@ void update_depth_buffer(int x, int y, float z) {
   buffer[y][x] = curr > z ? z : curr;
 }
 
-void draw_line(struct Vertex a, struct Vertex b) {
+void draw_line(struct Vec3 a, struct Vec3 b) {
   int cols, rows;
   getmaxyx(stdscr, rows, cols);
   int az = a.z;
@@ -72,49 +83,87 @@ void draw_line(struct Vertex a, struct Vertex b) {
     update_depth_buffer(ax, ay, az);
     az += step;
     if (ax == bx && ay == by) break;
-    int temp = error * 2;
-    if (temp > -deltaY) {
+    int vertices = error * 2;
+    if (vertices > -deltaY) {
       error -= deltaY;
       ax += signX;
     }
-    if (temp < deltaX) {
+    if (vertices < deltaX) {
       error += deltaX;
       ay += signY;
     }
   }
 }
 
+void draw_edges() {
+  int top = dot_vec3(camera, normales[0]);
+  int bottom = dot_vec3(camera, normales[1]);
+  int left = dot_vec3(camera, normales[2]);
+  int behind = dot_vec3(camera, normales[3]);
+  int right = dot_vec3(camera, normales[4]);
+  int front = dot_vec3(camera, normales[5]);
+
+  if (top < 0) {
+    draw_line(vertices[0], vertices[1]);
+    draw_line(vertices[1], vertices[2]);
+    draw_line(vertices[2], vertices[3]);
+    draw_line(vertices[3], vertices[0]);
+  }
+  if (bottom < 0) {
+    draw_line(vertices[4], vertices[5]);
+    draw_line(vertices[5], vertices[6]);
+    draw_line(vertices[6], vertices[7]);
+    draw_line(vertices[7], vertices[4]);
+  }
+  if (left < 0) {
+    draw_line(vertices[4], vertices[0]);
+    draw_line(vertices[0], vertices[1]);
+    draw_line(vertices[1], vertices[5]);
+    draw_line(vertices[5], vertices[4]);
+  }
+  if (behind < 0) {
+    draw_line(vertices[5], vertices[1]);
+    draw_line(vertices[1], vertices[2]);
+    draw_line(vertices[2], vertices[6]);
+    draw_line(vertices[6], vertices[5]);
+  }
+  if (right < 0) {
+    draw_line(vertices[7], vertices[3]);
+    draw_line(vertices[3], vertices[2]);
+    draw_line(vertices[2], vertices[6]);
+    draw_line(vertices[6], vertices[7]);
+  }
+  if (front < 0) {
+    draw_line(vertices[0], vertices[3]);
+    draw_line(vertices[3], vertices[7]);
+    draw_line(vertices[7], vertices[4]);
+    draw_line(vertices[4], vertices[0]);
+  }
+}
+
+void draw_faces() {}
+
 int main() {
   initscr();
   noecho();
   cbreak();
+
   while (1) {
-    struct Vertex temp[8];
-    memcpy(temp, vertices, sizeof(temp));
+    memcpy(vertices, base_vertices, sizeof(vertices));
     for (int i = 0; i < 8; i++) {
-      rotate_x(&temp[i]);
-      rotate_y(&temp[i]);
-      rotate_z(&temp[i]);
+      rotate_x(&vertices[i]);
+      rotate_y(&vertices[i]);
+      rotate_z(&vertices[i]);
     }
+
     for (int i = 0; i < MAX_ROWS; i++) {
       for (int j = 0; j < MAX_COLS; j++) {
         buffer[i][j] = far;
       }
     }
 
-    draw_line(temp[0], temp[1]);
-    draw_line(temp[1], temp[2]);
-    draw_line(temp[2], temp[3]);
-    draw_line(temp[3], temp[0]);
-    draw_line(temp[4], temp[5]);
-    draw_line(temp[5], temp[6]);
-    draw_line(temp[6], temp[7]);
-    draw_line(temp[7], temp[4]);
-    draw_line(temp[0], temp[4]);
-    draw_line(temp[1], temp[5]);
-    draw_line(temp[2], temp[6]);
-    draw_line(temp[3], temp[7]);
-
+    configure_normales();
+    draw_edges();
     int cols, rows;
     getmaxyx(stdscr, rows, cols);
     clear();
@@ -130,7 +179,6 @@ int main() {
 
     A += 0.1f;
     B += 0.1f;
-    C += 0.1f;
     usleep(100000);
   }
   endwin();
